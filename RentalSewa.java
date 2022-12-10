b6cc10f95037909802a5ade9be433e213a2cc203
Khulfani Hendrawan <101700561+Khulfani@users.noreply.github.com>
2022-12-10 19:54:23 +0700

    Abstrak Class

---
+++ RentalSewa.java
@@ -0,0 +1,9 @@
+public abstract class RentalSewa {
+    public int Biaya(int harga, int lama_sewa) {
+        return 0;
+    }
+    public void Jenis_PS(){
+        System.out.println("-");
+    }
+    abstract double getMoney();
+}
